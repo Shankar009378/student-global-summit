2f1f812864586d6fd4d2aafdfec28ce7c6bfa7c3
Shankar Kumar Nanda <nandashankar26@gmail.com>
2025-12-08 12:58:38 +0530

    SEO Optimised

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+import { NextSeo } from "next-seo";
+
 import Banners from "@/components/Banners";
 import Faq from "@/components/Faq";
 import Footer from "@/components/Footer";
@@ -14,6 +16,31 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
+      <NextSeo
+        title="Student Global Summit 2025"
+        description="India's biggest youth summit for innovation, entrepreneurship, technology, and leadership. Join the global movement shaping the future."
+        canonical="https://studentglobalsummit.com"
+        openGraph={{
+          url: "https://studentglobalsummit.com",
+          title: "Student Global Summit 2025",
+          description:
+            "Join India’s most dynamic youth summit featuring innovation showcases, global policy debates, startup exhibitions, and cultural confluence.",
+          images: [
+            {
+              url: "/icon.png",
+              width: 1200,
+              height: 630,
+              alt: "Student Global Summit 2025",
+            },
+          ],
+          siteName: "Student Global Summit 2025",
+        }}
+        twitter={{
+          handle: "@sgsummit2025",
+          site: "@sgsummit2025",
+          cardType: "summary_large_image",
+        }}
+      />
       <Banners />
       <HeroSection />
       <LogoClouds />
