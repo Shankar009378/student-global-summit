ff637eaf739255bcf64295986ed6721f70c7fd08
Shankar Kumar Nanda <nandashankar26@gmail.com>
2025-12-16 11:05:49 +0530

    Updated Code

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -52,7 +52,7 @@ export default function RegisterPage() {
         </div>
 
         {/* MAIN MESSAGE CARD */}
-        <div
+        {/* <div
           className="relative backdrop-blur-2xl bg-white/10 border border-white/20 
                      shadow-[0_0_50px_rgba(255,255,255,0.2)] 
                      rounded-3xl p-10 sm:p-14 text-center"
@@ -82,10 +82,10 @@ export default function RegisterPage() {
           <p className="mt-2 text-white/70 text-sm">
             Thank you for your patience.
           </p>
-        </div>
+        </div> */}
 
         {/* GLASS CONTAINER */}
-        {/* <div
+        <div
           className="relative backdrop-blur-2xl bg-white/10 border border-white 
             shadow-[0_0_40px_rgba(255,255,255,0.15)] 
             rounded-3xl p-10 sm:p-14 
@@ -95,7 +95,7 @@ export default function RegisterPage() {
           <div className="absolute inset-0 rounded-3xl pointer-events-none border border-white/10"></div>
 
           <FormLayout />
-        </div> */}
+        </div>
       </div>
     </section>
   );
